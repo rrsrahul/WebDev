f61b3d2c9b0dd4d4480719662fd8469bd5eb0b6a
Rahul R S <rahulrsgoku@gmail.com>
2020-09-24 17:48:49 +0530

    Trying out Functional Programming

--- React/Tutorials/practice/src/App.js
+++ React/Tutorials/practice/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Person from './Person/Person';
 
 import './App.css';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hi First React App</h1>
+      <Person/>
     </div>
   );
 }
---
+++ React/Tutorials/practice/src/Person/Person.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const person = ()=>
+{
+    return (
+    <h1>Functional React Programming</h1>
+    );
+}
+
+export default person;
