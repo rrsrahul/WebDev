47490855edb3fd555c3c823222dd9b289da78bde
Rahul R S <rahulrsgoku@gmail.com>
2020-09-28 20:35:19 +0530

    2 way binding

--- React/Tutorials/practice/src/App.js
+++ React/Tutorials/practice/src/App.js
@@ -3,6 +3,8 @@ import Person from './Person/Person';
 
 import './App.css';
 
+
+
 class App extends Component
 {
 
@@ -13,9 +15,71 @@ class App extends Component
       {name:'Heisen',age:22}
     ]
   }
-  switchNameHandler = ()=>
+  switchNameHandler = (newName)=>
   {
       this.setState({
+        persons:[
+          {name:'Rahul',age:10},
+          {name:'Rrs',age:12},
+          {name:newName,age:22}
+        ]
+      });
+      //Setstate merges the arguement passed with the State, and does not discard other state values
+  }
+  nameChangedHandler = (event)=>
+  {
+    this.setState({
+      persons:[
+        {name:'Rahul',age:10},
+        {name:event.target.value,age:12},
+        {name:'Heisen',age:22}
+      ]
+    });
+  }
+
+  render()
+  {
+    return (
+      <div className="App">
+         <h1>Functional React Programming</h1>
+        <h1>Hi First React App</h1>
+        <button onClick={() => this.switchNameHandler('HeisenbergswithArrowF')} >Switch</button>
+        <Person 
+        name={this.state.persons[0].name} 
+        age={this.state.persons[0].age}/>
+        <Person 
+        name={this.state.persons[1].name} 
+        age={this.state.persons[1].age}
+        changed={this.nameChangedHandler}
+        click ={this.switchNameHandler.bind(this,'Heisenbergs')} />
+        <Person 
+        name={this.state.persons[2].name} 
+        age={this.state.persons[2].age}/>
+       
+      </div>
+    );
+
+
+  }
+
+}
+
+/*
+React Hooks Syntax 
+const App = props =>
+{
+ const [personsState,setPersonState] = useState({
+    persons:[
+      {name:'Rahul',age:10},
+      {name:'Rrs',age:12},
+      {name:'Heisen',age:22}
+    ]
+  });
+
+  //does not merge the state, it replaces the old state data with the new state data
+  const switchNameHandler = ()=>
+  {
+      setPersonState({
         persons:[
           {name:'Rahul',age:10},
           {name:'Rrs',age:12},
@@ -25,24 +89,22 @@ class App extends Component
       //Setstate merges the arguement passed with the State, and does not discard other state values
   }
 
-  render()
-  {
     return (
       <div className="App">
          <h1>Functional React Programming</h1>
         <h1>Hi First React App</h1>
-        <button onClick={this.switchNameHandler} >Switch</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick={switchNameHandler} >Switch</button>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
+        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
        
       </div>
     );
 
 
-  }
-
+  
 }
+*/
 
 /*function App() {
   
--- React/Tutorials/practice/src/Person/Person.js
+++ React/Tutorials/practice/src/Person/Person.js
@@ -5,10 +5,10 @@ const person = (props)=>
     return (
     <div>
        
-        <p>
+        <p onClick={props.click}>
             Hi my name is {props.name} and my age is {props.age} 
         </p>
-        
+        <input type='text' onChange={props.changed} value={props.name}/>
         </div>
     
     );
