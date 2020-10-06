d84eb70a04964303941d334c66e934c1f803bb26
Rahul R S <rahulrsgoku@gmail.com>
2020-10-07 03:04:39 +0530

    Alternative Method for Hoc

--- React/Tutorials/practice/src/hoc/WithClass.js
+++ React/Tutorials/practice/src/hoc/WithClass.js
@@ -10,4 +10,26 @@ const WithClass = (props)=>
     );
 }
 
+/* Another method of using HigherOrderComponents, using a normal javaScript function
+
+const withClass =(WrappedComponent, classname)=>
+{
+    return props=>
+    {
+        return (
+            <div className = {classname}>
+                <WrappedComponent/>
+            </div>
+        )
+    }
+    
+}
+
+We can use this in App.js by 
+
+export default withClass(App.js,classes.App)
+
+
+
+*/
 export default WithClass;
