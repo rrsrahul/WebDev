99edf8ad58e8f1a07d7cd01ec47b61df194afbdf
Rahul R S <rahulrsgoku@gmail.com>
2020-10-25 03:23:18 +0530

    NavigationItems

--- React/Tutorials/burger/src/Components/Navigation/ToolBar/ToolBar.js
+++ React/Tutorials/burger/src/Components/Navigation/ToolBar/ToolBar.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import classes from './ToolBar.module.css'
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 const toolbar= (props)=>
 {
     return (
         <header className={classes.ToolBar}>
             <div>MENU</div>
             <Logo/>
-            <nav>Links</nav>
+            <nav>
+                <NavigationItems/>
+            </nav>
         </header>
     )
 }
