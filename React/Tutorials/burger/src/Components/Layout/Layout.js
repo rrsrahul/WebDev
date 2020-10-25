bedf90c65247a916aeb294ad08b4a3a19d5f1838
Rahul R S <rahulrsgoku@gmail.com>
2020-10-25 20:48:31 +0530

    Adding A SideDrawer

--- React/Tutorials/burger/src/Components/Layout/Layout.js
+++ React/Tutorials/burger/src/Components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import classes from './Layout.module.css'
 import ToolBar from '../../Components/Navigation/ToolBar/ToolBar'
+import SideDrawer from '../../Components/Navigation/SideDrawer/SideDrawer';
 const layout = (props)=>
 {
     return (<React.Fragment>
     <div><ToolBar/></div>
+    <SideDrawer/>
         <main className={classes.Content}> {props.children}</main>
         </React.Fragment>
         );
