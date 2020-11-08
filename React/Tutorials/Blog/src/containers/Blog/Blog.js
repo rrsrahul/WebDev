4245a3eb737e012b1bd0e7da1d96325c8e27486c
Rahul R S <rahulrsgoku@gmail.com>
2020-11-09 03:17:55 +0530

    Adding Links instead of <a>

--- React/Tutorials/Blog/src/containers/Blog/Blog.js
+++ React/Tutorials/Blog/src/containers/Blog/Blog.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 //import axios from 'axios';
 //import axios from '../../axios';
-import {Route} from 'react-router-dom';
+import {Route, Link} from 'react-router-dom';
 
 import Posts from '../Posts/Posts';
+import NewPost from '../NewPost/NewPost';
 
 import './Blog.css';
 
@@ -23,13 +24,19 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to={{
+                                pathname:'/new-post',
+                                hash: '#submit',
+                               
+                            }}>New Post</Link>
+                            </li>
                        
                         </ul>
                     </nav>
                 </header>
                 <Route  path='/' exact component={Posts}/>
+                <Route  path='/new-post' component={NewPost}/>
                 
                 
             </div>
