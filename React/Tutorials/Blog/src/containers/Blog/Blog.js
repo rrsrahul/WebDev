b82db6cc9d93a8b8458143afb667550e1ec94804
Rahul R S <rahulrsgoku@gmail.com>
2020-11-18 21:57:11 +0530

    LazyRouting

--- React/Tutorials/Blog/src/containers/Blog/Blog.js
+++ React/Tutorials/Blog/src/containers/Blog/Blog.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
 //import axios from 'axios';
 //import axios from '../../axios';
-import {Route, NavLink,Switch,Redirect} from 'react-router-dom';
+import {Route, NavLink,Switch} from 'react-router-dom';
+import './Blog.css';
 
 import Posts from '../Posts/Posts';
-import NewPost from '../NewPost/NewPost';
+//import NewPost from '../NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+
+const AsyncNewPost = asyncComponent(()=>
+{
+    return import('../NewPost/NewPost')
+});
 
 
-import './Blog.css';
+
 
 
 class Blog extends Component {
     state = 
     {
-        auth:false
+        auth:true
     }
     render () {
 
@@ -42,7 +49,7 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Switch>
-                    {this.state.auth ? <Route  path='/new-post' component={NewPost}/>:null}
+                    {this.state.auth ? <Route  path='/new-post' component={AsyncNewPost}/>:null}
                     <Route  path='/posts'  component={Posts}/>
                     <Route render={()=><h1>Not Found</h1>}/>
                     {/*<Route  path='/'  component={Posts}/> Use Redirect instead of this*/}
