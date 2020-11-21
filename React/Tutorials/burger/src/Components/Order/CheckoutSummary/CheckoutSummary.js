9fbb48b8675807778f99372c0bbb25ab25abfacf
Rahul R S <rahulrsgoku@gmail.com>
2020-11-22 02:08:28 +0530

    Adding Navigation and Query Params

--- React/Tutorials/burger/src/Components/Order/CheckoutSummary/CheckoutSummary.js
+++ React/Tutorials/burger/src/Components/Order/CheckoutSummary/CheckoutSummary.js
@@ -9,13 +9,17 @@ const checkoutSummary = (props)=>
     return (<div className={classes.CheckoutSummary}>
         <h1>Hope it tastes good</h1>
         <div style={{width:'100%',margin:'auto'}}>
-        <Burger ingredients={props.ingredients}/>
+        <Burger ingredients={props.ingredients}
+        />
         </div>
         <Button 
-        btnType="Danger">Cancel</Button>
+        btnType="Danger"
+        clicked={props.checkoutCancelled}
+        >Cancel</Button>
 
         <Button 
-        btnType="Success">Continue</Button>
+        btnType="Success"
+        clicked={props.checkoutSuccess}>Continue</Button>
     </div>)
 
 }
--- React/Tutorials/burger/src/Containers/Checkout/Checkout.js
+++ React/Tutorials/burger/src/Containers/Checkout/Checkout.js
@@ -12,10 +12,39 @@ class Checkout extends Component{
             cheese:1
         }
     }
+
+    componentDidMount()
+    {
+        const query = new URLSearchParams(this.props.location.search);
+        //console.log(query);
+        const ingredients = {};
+        for(let param of query.entries())
+        {
+            //[salad,'1']
+            console.log(param);
+            ingredients[param[0]] = +param[1]
+        }
+
+        this.setState({ingredients:ingredients});
+    }
+
+    checkoutCancelledHandler = ()=>
+    {
+        this.props.history.goBack();
+    }
+
+    checkoutSuccessHandler = ()=>
+    {
+        this.props.history.replace('/checkout/contact-data');
+    }
+
+
     render()
     {
         return <div>
-            <CheckoutSummary ingredients={this.state.ingredients}/>
+            <CheckoutSummary ingredients={this.state.ingredients}
+            checkoutCancelled={this.checkoutCancelledHandler}
+            checkoutSuccess={this.checkoutSuccessHandler}/>
         </div>
     }
 }
