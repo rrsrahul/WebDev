1f29cf1da11f08cba99b28eebaf0dbca3d35490d
Rahul R S <rahulrsgoku@gmail.com>
2020-11-26 02:29:57 +0530

    FeedBack of Form Validation

--- React/Tutorials/burger/src/Components/UI/Input/Input.js
+++ React/Tutorials/burger/src/Components/UI/Input/Input.js
@@ -6,11 +6,17 @@ import classes from './Input.module.css'
 const input = (props)=>
 {
     let inputElement = null;
+    const inputClasses = [classes.InputElement];
+
+    if(props.invalid === true && props.shouldValidate)
+    {
+        inputClasses.push(classes.Invalid);
+    }
 
     switch(props.elementType)
     {
         case('input'):
-            inputElement=<input className={classes.InputElement} 
+            inputElement=<input className={inputClasses.join(' ')} 
             {...props.elementConfig} 
             value={props.value}
             onChange={props.changed}/>
