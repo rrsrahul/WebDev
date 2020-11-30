3310ba2cf68cca88f252f9795848353c16c40f48
Rahul R S <rahulrsgoku@gmail.com>
2020-12-01 01:44:48 +0530

    Dispactching Actions with Redux

--- React/Tutorials/ReduxPractice/redux-basics.js
+++ React/Tutorials/ReduxPractice/redux-basics.js
@@ -11,6 +11,22 @@ const initialState = {
 //Reducer
 const rootReducer = ( state= initialState , action)=>
 {
+    if(action.type ==='INC_COUNTER')
+    {
+        return {
+            ...state,
+            counter: state.counter+1
+        }
+    }
+
+    if(action.type==='ADD_COUNTER')
+    {
+        return {
+            ...state,
+            counter:state.counter + action.value
+        }
+    }
+
     //return updated State
 
     return state;
@@ -26,5 +42,8 @@ console.log(store.getState());
 
 //Dispatching Action
 
+store.dispatch({type:'INC_COUNTER'});
+store.dispatch({type:'ADD_COUNTER',value:10});
+console.log(store.getState());
 
 //Subscription
