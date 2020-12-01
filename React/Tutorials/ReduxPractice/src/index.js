9cbd59d847ca5deeca3a28295c31594329b8b73b
Rahul R S <rahulrsgoku@gmail.com>
2020-12-02 02:35:08 +0530

    Connecting React to Redux

--- React/Tutorials/ReduxPractice/src/index.js
+++ React/Tutorials/ReduxPractice/src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {createStore} from 'redux'
+import reducer from './store/reducer'
+
+
+//Takes Reducer as an input
+const store = createStore(reducer);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
