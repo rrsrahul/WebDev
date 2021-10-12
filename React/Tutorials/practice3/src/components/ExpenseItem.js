e34c8aebccd4e9e09f5de80495c4779eaf14aeff
Rahul R S <rahulrsgoku@gmail.com>
2021-10-13 00:59:29 +0530

    CSS

--- React/Tutorials/practice3/src/components/ExpenseItem.js
+++ React/Tutorials/practice3/src/components/ExpenseItem.js
@@ -1,6 +1,15 @@
+import './ExpenseItem.css'
 
-function ExpenseItem () {
-    return (<h2>Expense Item</h2>)
+function ExpenseItem() {
+    return (
+        <div className="expense-item">
+            <div>Date</div>
+            <div className="expense-item__description">
+                <h2>Insurance</h2>
+                <div className="expense-item__price">Amount</div>
+            </div>
+
+        </div>)
 }
 
-export default ExpenseItem; 
+export default ExpenseItem;
