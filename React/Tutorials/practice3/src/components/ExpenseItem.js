fab8fb32a566d03bb758c623dcf6a6537847f3e3
Rahul R S <rahulrsgoku@gmail.com>
2021-10-20 13:57:10 +0530

    minor Changes

--- React/Tutorials/practice3/src/components/ExpenseItem.js
+++ React/Tutorials/practice3/src/components/ExpenseItem.js
@@ -1,8 +1,15 @@
+//import {useState} from 'react'
 import ExpenseDate from './ExpenseDate';
 import Card from './Card';
 import './ExpenseItem.css'
 
+const onClickHandler = () => {
+    console.log('Clicked')
+}
+
+
 function ExpenseItem(props) {
+
 
 
     return (
@@ -11,6 +18,7 @@ function ExpenseItem(props) {
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">{props.amount}</div>
+                <button onClick={onClickHandler}>Click me</button>
             </div>
         </Card>)
 }
