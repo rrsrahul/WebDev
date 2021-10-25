8fe3f89b4b79c50a5993bdcde7762f0198a2bb15
Rahul R S <rahulrsgoku@gmail.com>
2021-10-26 01:49:39 +0530

    2 way Binding

--- React/Tutorials/practice3/src/components/ExpenseItems.js
+++ React/Tutorials/practice3/src/components/ExpenseItems.js
@@ -3,8 +3,9 @@ import Card from "./Card"
 
 function ExpenseItems(props) {
 
-    let expenseItems = props.expenses.map((expenseItem) => {
+    let expenseItems = props.expenses.map((expenseItem,idx) => {
         return (<ExpenseItem date={expenseItem.date}
+            key={idx}
             amount={expenseItem.amount}
             title={expenseItem.title} />)
     })
