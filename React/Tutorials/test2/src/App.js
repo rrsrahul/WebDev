bd3bc285eaf35dd54e9b0f8024fa9ea783e3c40a
Rahul R S <rahulrsgoku@gmail.com>
2021-11-10 15:57:43 +0530

    init

--- React/Tutorials/test2/src/App.js
+++ React/Tutorials/test2/src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
+const calcStyle = (value) => {
+
+
+  if (value >= 1 && value <= 4)
+    return '#33D7FF'
+  if (value >= 5 && value <= 7)
+    return '#33FFC2'
+  if (value >= 8 && value <= 10)
+    return '#33FF71'
+  if (value >= 11 && value <= 13)
+    return '#D9FF33'
+  if (value >= 14)
+    return '#FF5A33'
+  if (value < 0 && value >= -3)
+    return '#339BFF'
+  if (value <= -4 && value >= -7)
+    return '#334AFF'
+  if (value <= -8)
+    return '#E233FF'
+
+}
+
 function App() {
+
+
+
+  const [number, updateNumber] = useState(0)
+  const increaseHandler = () => {
+    updateNumber((prevState) => prevState + 1)
+  }
+
+  const decreaseHandler = () => {
+    updateNumber((prevState) => prevState - 1)
+  }
+
+  const increaseDecreaseHandler = () => {
+    //const initial = number;
+    increaseHandler();
+    setTimeout(decreaseHandler, 1500)
+
+  }
+
+  const decreaseIncreaseHandler = () => {
+
+    decreaseHandler();
+    setTimeout(increaseHandler, 2000)
+
+  }
+  const color = calcStyle(number);
+  console.log(color)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={increaseHandler}>Increase</button>
+      <button onClick={decreaseHandler}>Decrease</button>
+      <button onClick={increaseDecreaseHandler}>Increase-Decrease</button>
+      <button onClick={decreaseIncreaseHandler}>Decrease-Increase</button>
+      <p style={{ 'color':color, fontSize: '2rem' }}>{number}</p>
     </div>
   );
 }
 
 export default App;
+
